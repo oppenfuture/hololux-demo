b0fd5091ed7ce4b7028c770431b9fc2b17255a95
chubei <chubei@oppentech.com>
2021-11-04 11:06:27 +0800

    Minor fix: Add return for main in build test

--- HololuxScreenshotBuildTest.cpp
+++ HololuxScreenshotBuildTest.cpp
@@ -8,4 +8,5 @@ int main() {
   app->loadModel("data/Adidas_shoe_43287072668.hlf");
   std::vector<uint8_t> data(4);
   app->getImageByAngle(data.data(), 1, 1, 0.0f, {0.0f, 0.0f, 0.0f, 0.0f});
+  return 0;
 }
